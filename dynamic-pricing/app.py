f5b3e7f8b3b71779d150491b2171f51876a4c1c3
Anurag Singh <anuragsingh815311@gmail.com>
2026-03-27 19:40:04 +0530

    Adding basic flask structure for testing

---
+++ dynamic-pricing/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl", "rb"))
+encoder = pickle.load(open("encoder.pkl", "rb"))
+features = pickle.load(open("features.pkl", "rb"))
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        input_data = []
+
+        for feature in features:
+            if feature == "season":
+                val = encoder.transform([request.form[feature]])[0]
+            else:
+                val = float(request.form[feature])
+
+            input_data.append(val)
+
+        prediction = model.predict([input_data])[0]
+
+        return render_template("index.html",
+            result=f"Predicted Price Difference: ₹ {round(prediction,2)}")
+
+    except Exception as e:
+        return render_template("index.html",
+            result=f"Error: {str(e)}")
+
+if __name__ == "__main__":
+    app.run(debug=True)
